6f532e5fe3e8ed689f6f290321235221c2680fd9
pedrogao <gaopedro@163.com>
2023-08-16 07:57:53 +0000

    Deploying to gh-pages from @ pedrogao/pedrogao.github.io@bfe5a7d79f0ee3ead4df708b5b0fd14dbc82a133 🚀

---
+++ assets/cmu15445-6.html.8fe87f17.js
@@ -0,0 +1,39 @@
+import{_ as s}from"./plugin-vue_export-helper.21dcd24c.js";import{r,o as a,c as d,a as e,d as i,e as c,b as l}from"./app.5099eb31.js";const o={},u=c(`<h2 id="\u65E5\u5FD7\u548C\u6062\u590D" tabindex="-1"><a class="header-anchor" href="#\u65E5\u5FD7\u548C\u6062\u590D" aria-hidden="true">#</a> \u65E5\u5FD7\u548C\u6062\u590D</h2><p>\u8BA1\u7B97\u673A\u5BB9\u6613\u53D1\u751F\u5404\u79CD\u6545\u969C\uFF0C\u5982\u78C1\u76D8\u6545\u969C\u3001\u5B95\u673A\u3001\u8F6F\u4EF6\u9519\u8BEF\u7B49\u3002\u4E00\u65E6\u8BA1\u7B97\u673A\u6545\u969C\u5C31\u5BB9\u6613\u5F15\u8D77\u8FD0\u884C\u5176\u5185\u7684\u6570\u636E\u5E93\u4E22\u5931\u6570\u636E\uFF0C\u56E0\u6B64\u6570\u636E\u5E93\u5FC5\u987B\u9884\u5148\u91C7\u53D6\u63AA\u65BD\u4EE5\u4FDD\u8BC1\u5373\u4F7F\u53D1\u751F\u6545\u969C\uFF0C\u6570\u636E\u4ECD\u7136\u627E\u56DE\u3002</p><p>\u6062\u590D\u673A\u5236\u662F\u6570\u636E\u5E93\u5FC5\u4E0D\u53EF\u5C11\u7684\u4E00\u90E8\u5206\uFF0C\u5B83\u80FD\u4FDD\u8BC1\u6570\u636E\u5E93\u5373\u4F7F\u5728\u6545\u969C\u53D1\u751F\u7684\u60C5\u51B5\u4E0B\uFF0C\u4ECD\u7136\u6062\u590D\u5230\u53D1\u751F\u524D\u7684\u72B6\u6001\uFF0C\u4FDD\u8BC1\u524D\u540E\u6570\u636E\u7684\u4E00\u81F4\u6027\uFF0C\u4FDD\u8BC1\u6570\u636E\u5E93\u7684\u9AD8\u53EF\u7528\u6027\u3002</p><p>\u6062\u590D\u7B97\u6CD5\u662F\u786E\u4FDD\u6570\u636E\u5E93\u4E00\u81F4\u6027\u3001\u539F\u5B50\u6027\u548C\u6301\u4E45\u6027\u7684\u5173\u952E\u6280\u672F\u3002</p><p>\u6062\u590D\u7B97\u6CD5\u4E3B\u8981\u5DE5\u4F5C\u5305\u62EC\u5982\u4E0B\u4E24\u90E8\u5206\uFF1A</p><ul><li><p>\u8BB0\u5F55\u4E8B\u52A1\u5904\u7406\u671F\u95F4\u7684\u64CD\u4F5C\uFF0C\u4EE5\u786E\u4FDD DBMS \u53EF\u4EE5\u4ECE\u6545\u969C\u4E2D\u6062\u590D\uFF1B</p></li><li><p>\u5C06\u6570\u636E\u5E93\u6062\u590D\u5230\u67D0\u4E2A\u5931\u8D25\u540E\u7684\u72B6\u6001\uFF0C\u786E\u4FDD\u64CD\u4F5C\u7684\u539F\u5B50\u6027\u3001\u4E00\u81F4\u6027\u548C\u6301\u4E45\u6027\u3002 \u6062\u590D\u7B97\u6CD5\u4E3B\u8981\u601D\u8DEF\uFF1A</p></li><li><p>\u9884\u5199\u65E5\u5FD7(WAL)\uFF1A</p><ul><li>\u4EFB\u4F55\u6570\u636E\u66F4\u6539\u5728\u5E94\u7528\u5230\u6570\u636E\u5E93\u4E4B\u524D\uFF0C\u5FC5\u987B\u5148\u7531\u65E5\u5FD7\u5199\u5165\u78C1\u76D8\u3002</li><li>\u5FC5\u987B\u4F7F\u7528 STEAL + NO-FORCE \u7F13\u51B2\u6C60\u7B56\u7565\u3002</li></ul></li><li><p>\u5728\u91CD\u505A\u671F\u95F4\u6062\u590D\u5386\u53F2\u6570\u636E\uFF1A\u5728\u91CD\u65B0\u542F\u52A8\u65F6\uFF0C\u91CD\u505A\u64CD\u4F5C\u5E76\u5C06\u6570\u636E\u5E93\u6062\u590D\u5230\u5D29\u6E83\u524D\u7684\u72B6\u6001\u3002</p></li><li><p>\u64A4\u6D88\u671F\u95F4\u8BB0\u5F55\u66F4\u6539\uFF1A\u5C06\u64A4\u6D88\u64CD\u4F5C\u8BB0\u5F55\u5230\u65E5\u5FD7\u4E2D\u4EE5\u786E\u4FDD\u64CD\u4F5C\u4E0D\u4F1A\u5728\u91CD\u590D\u5931\u8D25\u7684\u60C5\u51B5\u4E0B\u91CD\u590D\u6267\u884C\u3002</p></li></ul><h3 id="\u65E5\u5FD7" tabindex="-1"><a class="header-anchor" href="#\u65E5\u5FD7" aria-hidden="true">#</a> \u65E5\u5FD7</h3><p>\u5728 bustub \u4E2D\u901A\u8FC7 LogManager \u6765\u5B9E\u73B0WAL\u65E5\u5FD7\u8BB0\u5F55\u3002\u7C7B\u5B9A\u4E49\u5982\u4E0B\uFF1A</p><div class="language-c++ ext-c++ line-numbers-mode"><pre class="language-c++"><code>class LogManager {
+ public:
+  explicit LogManager(DiskManager *disk_manager)
+      : next_lsn_(0), persistent_lsn_(INVALID_LSN), disk_manager_(disk_manager) {
+    log_buffer_ = new char[LOG_BUFFER_SIZE];
+    flush_buffer_ = new char[LOG_BUFFER_SIZE];
+  }
+  ~LogManager() {
+    delete[] log_buffer_;
+    delete[] flush_buffer_;
+    log_buffer_ = nullptr;
+    flush_buffer_ = nullptr;
+  }
+  void RunFlushThread();
+  void StopFlushThread();
+  void Flush(bool force);
+  lsn_t AppendLogRecord(LogRecord *log_record);
+  inline lsn_t GetNextLSN() { return next_lsn_; }
+  inline lsn_t GetPersistentLSN() { return persistent_lsn_; }
+  inline void SetPersistentLSN(lsn_t lsn) { persistent_lsn_ = lsn; }
+  inline char *GetLogBuffer() { return log_buffer_; }
+  // ...
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="\u6062\u590D" tabindex="-1"><a class="header-anchor" href="#\u6062\u590D" aria-hidden="true">#</a> \u6062\u590D</h3><p>\u90A3\u4E48 bustub \u78B0\u5230\u6570\u636E\u5E93\u5D29\u6E83\u3001\u7535\u6E90\u65AD\u5F00\u7B49\u60C5\u51B5\u53C8\u8BE5\u5982\u4F55\u6062\u590D\u5462\uFF1Fbustub \u7684\u5904\u7406\u5176\u5B9E\u662F\u6BD4\u8F83\u7C97\u7CD9\u7684\uFF0C\u5B9A\u4E49\u5982\u4E0B\uFF1A</p><div class="language-c++ ext-c++ line-numbers-mode"><pre class="language-c++"><code>class LogRecovery {
+ public:
+  LogRecovery(DiskManager *disk_manager, BufferPoolManager *buffer_pool_manager)
+      : disk_manager_(disk_manager), buffer_pool_manager_(buffer_pool_manager), offset_(0) {
+    log_buffer_ = new char[LOG_BUFFER_SIZE];
+  }
+  ~LogRecovery() {
+    delete[] log_buffer_;
+    log_buffer_ = nullptr;
+  }
+  void Redo();
+  void Undo();
+  bool DeserializeLogRecord(const char *data, LogRecord *log_record);
+  // ...
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>bustub \u7684\u5D29\u6E83\u6062\u590D\u5FC5\u987B\u5148 Redo \u518D Undo\uFF0C\u9996\u5148 Redo \u4F1A\u626B\u63CF\u5168\u90E8\u7684\u65E5\u5FD7\u6587\u4EF6\uFF0C\u5E76\u901A\u8FC7 LSN \u5927\u5C0F\u6765\u91CD\u505A\u672A\u5B8C\u6210\u7684\u4E8B\u52A1\uFF0C\u5728\u91CD\u505A\u671F\u95F4\u8BB0\u5F55\u6B63\u5728\u6D3B\u8DC3\u7684\u4E8B\u52A1\uFF0C\u7136\u540E\u8C03\u7528 Undo \u6765\u64A4\u9500\u8FD9\u4E9B\u64CD\u4F5C\u3002</p><h3 id="\u53C2\u8003\u8D44\u6599" tabindex="-1"><a class="header-anchor" href="#\u53C2\u8003\u8D44\u6599" aria-hidden="true">#</a> \u53C2\u8003\u8D44\u6599</h3>`,14),t={href:"https://15445.courses.cs.cmu.edu/fall2019/project4/",target:"_blank",rel:"noopener noreferrer"},v=l("PROJECT #4 - LOGGING & RECOVERY"),_=e("li",null,"Database System Concepts 16",-1),b={href:"https://15445.courses.cs.cmu.edu/fall2019/slides/21-recovery.pdf",target:"_blank",rel:"noopener noreferrer"},m=l("Database Recovery");function p(f,h){const n=r("ExternalLinkIcon");return a(),d("div",null,[u,e("ul",null,[e("li",null,[e("a",t,[v,i(n)])]),_,e("li",null,[e("a",b,[m,i(n)])])])])}var R=s(o,[["render",p],["__file","cmu15445-6.html.vue"]]);export{R as default};
