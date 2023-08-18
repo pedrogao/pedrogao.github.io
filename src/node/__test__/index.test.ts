da0a178990f8350b5af51d63cc8ecb6d8cb4c2da
pedrogao <1312342604@qq.com>
2023-08-18 22:20:37 +0800

    feat: add test tools

---
+++ src/node/__test__/index.test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from 'vitest';
+
+test('add', () => {
+  expect(1 + 1).toBe(2);
+  expect('map'.slice(1)).toMatchSnapshot('"ap"');
+});
--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,10 +1,16 @@
 import { createServer } from 'vite';
 import { pluginIndexHtml } from './plugin-island/indexHtml';
 import pluginReact from '@vitejs/plugin-react';
+import { PACKAGE_ROOT } from './constants';
 
 export async function createDevServer(root = process.cwd()) {
   return createServer({
     root,
-    plugins: [pluginIndexHtml(), pluginReact()]
+    plugins: [pluginIndexHtml(), pluginReact()],
+    server: {
+      fs: {
+        allow: [PACKAGE_ROOT]
+      }
+    }
   });
 }
